0a160163cfc18c26adeabde9732c6b0e2b45bee7
marc88chen <66193562+marc88chen@users.noreply.github.com>
2024-01-18 12:29:06 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -275,15 +275,15 @@ class CarInterface(CarInterfaceBase):
     ret.minEnableSpeed = -1. if (stop_and_go or ret.enableGasInterceptor) else MIN_ACC_SPEED
 
     # on stock Toyota this is -2.5
-    ret.stopAccel = -2.5
+    ret.stopAccel = -2.0
     ret.stoppingDecelRate = 0.2
 
     tune = ret.longitudinalTuning
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [.0, .15]
     if candidate in TSS2_CAR:
-      ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.25
+      ret.vEgoStopping = 0.1
+      ret.vEgoStarting = 0.1
       ret.stoppingDecelRate = 0.05  # reach stopping target smoothly
     tune.kpBP = [0.,]
     tune.kiBP = [0., 3.]
