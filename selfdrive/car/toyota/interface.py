ef4a048bbc80c9f86e9aedf2a313eb6b1c7cf83c
marc88chen <66193562+marc88chen@users.noreply.github.com>
2024-01-24 19:38:41 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -281,7 +281,7 @@ class CarInterface(CarInterfaceBase):
     if candidate in TSS2_CAR:
       ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
-      ret.stoppingDecelRate = 0.3  # reach stopping target quicker on TSS2.0
+      ret.stoppingDecelRate = 0.1  # reach stopping target quicker on TSS2.0
     tune.kpBP = [0.]
     tune.kpV = [1.]
     tune.kiBP = [0.]
