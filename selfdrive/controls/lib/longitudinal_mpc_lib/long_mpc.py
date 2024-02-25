4d1d6bc0d89976ebd4fb5511ab7891001256f499
marc88chen <66193562+marc88chen@users.noreply.github.com>
2024-02-25 20:47:45 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -89,7 +89,7 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
     x_vel =  [0.0,  5.0,   13.90,  20,    25,    40]
     y_dist = [1.1,  1.1,   1.3,    1.45,  1.6,  1.6]
   elif personality==log.LongitudinalPersonality.aggressive:
-    x_vel =  [0.0,  5.0,   13.89,  16.,   20,    25,    40]
+    x_vel =  [0.0,  5.0,   12.00,  15.,   20,    25,    40]
     y_dist = [1.05, 1.10,  1.20,   1.20,  1.25,  1.25,   1.3]
   else:
     raise NotImplementedError("Dynamic Follow personality not supported")
