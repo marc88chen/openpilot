b65a386f0fa509076516946f6efe4b6058eadbd7
marc88chen <66193562+marc88chen@users.noreply.github.com>
2025-10-23 23:38:25 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -88,7 +88,7 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
   # The Dynamic follow function is adjusted by Marc(cgw1968-5779)
   if personality==log.LongitudinalPersonality.relaxed:
     x_vel =  [0.,  6,   10., 10.01, 15., 27.7]
-    y_dist = [1.8, 1.9, 2.0,  2.0, 2.0, 2.2]
+    y_dist = [2.0, 2.0, 2.0,  2.0, 2.1, 2.2]
   elif personality==log.LongitudinalPersonality.standard:
     x_vel =  [0.,  6,   10., 10.01, 15., 27.7]
     y_dist = [1.3, 1.4, 1.4, 1.42,  1.45, 1.50]
