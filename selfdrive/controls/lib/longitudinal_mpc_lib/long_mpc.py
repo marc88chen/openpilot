85e9c816bf910ba2c4cfab2545a117f1483fbf37
marc88chen <66193562+marc88chen@users.noreply.github.com>
2025-10-23 23:39:15 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -102,7 +102,7 @@ def get_dynamic_follow(v_ego, personality=log.LongitudinalPersonality.standard):
 
 def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 6.5
+    return 7.5
   elif personality==log.LongitudinalPersonality.standard:
     return 6.0
   elif personality==log.LongitudinalPersonality.aggressive:
